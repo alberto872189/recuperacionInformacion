de2fc3daab09c9c8546a0c8d7e98e01eda3bdb36
alberto872189 <872189@unizar.es>
2025-11-04 16:44:37 +0100

    practica 4

---
+++ practica4/clasificadorTexto.py
@@ -0,0 +1,126 @@
+import os
+import sys
+import random
+import pandas as pd, numpy as np, commonFunctions
+from commonFunctions import to_categorical, cleanTexts, TextVectorization
+from textClassifier__DataReader import dataReader
+import xml.etree.ElementTree as ET
+import textClassifier_Transformer_main as model
+import textClassifier__TrainerTester as trainertester
+
+categories = ['Ingeniería', 'Ciencias de la Salud', 'Artes y Humanidades', 'Ciencias', 'Ciencias sociales y jurídicas']
+
+relations = {
+    'Ingeniería': ['Arquitectura', 'Defensa y Seguridad', 'Agroalimentaria y del Medio Rural', 'Biomédica', 'Civil', 'Industrial', 'Telecomunicación', 'Eléctrica', 'Electrónica', 'Informática', 'Mecánica', 'Mecatrónica', 'Ingeniería Química'],
+    'Ciencias de la Salud': ['Enfermería', 'Fisioterapia', 'Medicina', 'Nutrición', 'Odontología', 'Psicología', 'Terapia Ocupacional', 'Veterinaria'],
+    'Artes y Humanidades': ['Bellas Artes', 'Estudios Clásicos', 'Estudios Ingleses', 'Filología Hispánica', 'Filosofía', 'Historia', 'Lenguas Modernas'],
+    'Ciencias': ['Biotecnología', 'Ciencia', 'Física', 'Geología', 'Matemáticas', 'Óptica', 'Química'],
+    'Ciencias sociales y jurídicas': ['Administración', 'Deporte', 'Derecho', 'Economía', 'Empresa', 'Finanzas', 'Geografía', 'Gestión', 'Información', 'Educación', 'Marketing', 'Periodismo', 'Relaciones Laborales', 'Trabajo Social', 'Turismo']
+}
+
+def generate_datasets(input_dir):
+    valid_documents = []
+    #file_path = os.path.join(foldername, filename)
+    # print(file_path)
+    for doc in os.scandir(input_dir):
+        file_path = doc.path
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        raw_text = "".join(root.itertext())
+        # break into lines and remove leading and trailing space on each
+        text = ' '.join(line.strip() for line in raw_text.splitlines() if line)
+
+        # Obtener el diccionario de namespaces
+        namespaces = dict([
+            node for _, node in ET.iterparse(file_path, events=['start-ns'])
+        ])
+
+        # Mostrar la URI asociada al prefijo 'dc'
+        dc_uri = namespaces.get('dc')
+
+        doc_type = ''
+        title = ''
+        description = ''
+        subject = -1
+        solved = False
+        for child in root:
+            if child.tag == f'{{{dc_uri}}}type':
+                if child.text: doc_type += child.text
+            if child.tag == f'{{{dc_uri}}}description':
+                if child.text: description += child.text
+            if child.tag == f'{{{dc_uri}}}title':
+                if child.text: title += child.text
+            if child.tag == f'{{{dc_uri}}}subject':
+                i = 0
+                for clave in categories:
+                    for valor in relations[clave]:
+                        if valor in child.text and not solved:
+                            subject = i
+                            solved = True
+                            break
+                    i += 1
+                    if solved: break
+            
+
+        if (doc_type != 'TAZ-TFG'):
+            continue
+
+        if not solved: print(file_path, '\n')
+        valid_documents.append((file_path, title, description, subject))
+    training_set = random.sample(valid_documents, k=int(0.8*len(valid_documents)))
+    test_set = list(set(valid_documents).difference(training_set))
+
+    return training_set, test_set
+    
+        
+input_dir = '../recordsdc'
+output_dir = 'outputs'
+
+i = 1
+while i < len(sys.argv):
+    if sys.argv[i] == '-dir':
+        input_dir = sys.argv[i + 1]
+        i += 1
+    elif sys.argv[i] == '-output':
+        output_dir = sys.argv[i + 1]
+        i += 1
+    i += 1
+
+training_set, test_set = generate_datasets(input_dir)
+
+def transform_list_to_pd(my_list):
+    df = pd.DataFrame(my_list, columns=['File', 'Title', 'Description', 'Subject'])
+
+    # Crear la columna 'Text' concatenando Title y Description
+    df['Text'] = df['Title'] + '. ' + df['Description']
+
+    cleanedTexts = cleanTexts((df['Text'].values))
+
+    # Crear la columna 'Class Index' a partir de Subject
+    df['Class Index'] = df['Subject']
+
+    # Seleccionar solo las dos columnas que quieres
+    df = df[['Class Index', 'Text']]
+    return df
+
+training_set = transform_list_to_pd(training_set)
+test_set = transform_list_to_pd(test_set)
+
+# Para el training set
+X_train = training_set['Text']
+y_train = training_set['Class Index']
+
+# Para el test set
+X_test = test_set['Text']
+y_test = test_set['Class Index']
+
+vectorizer = TextVectorization(max_tokens=None,output_mode='int', output_sequence_length=200)
+vectorizer.adapt(X_train)
+X_train = vectorizer(X_train)
+X_test = vectorizer(X_test)
+y_train = to_categorical(training_set['Class Index'].values) # type: ignore
+y_test = to_categorical(test_set['Class Index'].values) # type: ignore
+
+my_model = model.createModel(200, 10)
+
+trainertester.trainerTester(my_model, [X_train, y_train, X_test, y_test], 10, 'misresultados')
---
+++ practica4/textClassifier__DataReader.py
@@ -0,0 +1,51 @@
+#-------------------------------------------------------------------------------
+# Método de lectura y procesamiento de datos usados en los clasificadores de texto.
+# Incluye las funciones auxiliares usadas por este método.
+#-------------------------------------------------------------------------------
+
+# Importaciones requeridas.
+import pandas as pd, numpy as np
+from commonFunctions import cleanTexts, to_categorical, TextVectorization
+
+
+#-------------------------------------------------------------------------------
+# Método para leer los ficheros tabulares del ejemplo de clasificación de texto (clasificación, título y descripción)
+# Lee un fichero en un dataframe de Pandas y junta el título con la descripción
+#-------------------------------------------------------------------------------
+def __readDataframe(file):
+    df = pd.read_csv(file, index_col=False)
+    df['Text'] = df['Title'] + '. ' + df['Description']
+    df.drop(['Title', 'Description'], axis=1, inplace=True)
+    return df
+
+#-------------------------------------------------------------------------------
+# Devuelve los datos de entrenamiento y test del clasificador de texto, limpios, segmentados, transformados a codificación numérica y
+# normalizados entre 0 y 1 cuando es necesario. Las categorías las convierte a la representación one-hot.
+# Selecciona un fracción de los datos (aleatorio). Esto esta hecho en el ejercicio para que el entrenamiento sea más rápido a costa de precisión.
+#-------------------------------------------------------------------------------
+def dataReader(fraction = 1, normalize = False, input_dir = '../recordsdc'):
+    # Cargamos los datos de entrenamiento y test.
+    trainingDataset = __readDataframe('data/clasificacionEntrenamiento.csv')
+    trainingDataset = trainingDataset.sample(frac=fraction, random_state=0)
+    testDataset = __readDataframe('data/clasificacionTest.csv')
+    
+    #Limpiamos los textos de caracteres no alfanuméricos.
+    X_train = cleanTexts(trainingDataset['Text'].values)
+    X_test = cleanTexts(testDataset['Text'].values)
+    
+    #Convertimos el texto en secuencias numéricas y las categorías a representación one-hot.
+    vectorizer = TextVectorization(max_tokens=None,output_mode='int', output_sequence_length=200)
+    vectorizer.adapt(X_train)
+    X_train = vectorizer(X_train)
+    X_test = vectorizer(X_test)
+    y_train = to_categorical(trainingDataset['Class Index'].values - 1) # type: ignore
+    y_test = to_categorical(testDataset['Class Index'].values - 1) # type: ignore
+    
+    
+    # Si hay que normalizar las Xs, las pasamos al rango 0-1.
+    if normalize:
+        X_train = (X_train - np.min(X_train)) / (np.max(X_train) - np.min(X_train))
+        X_test = (X_test - np.min(X_test)) / (np.max(X_test) - np.min(X_test))
+    
+    # Se devuelven las colecciones de entrenamiento y test empaquetadas, la longitud  de las frases de entrada, y el vocabulario de palabras conocidas.                
+    return (X_train, y_train, X_test, y_test), len(X_train[0]), len(vectorizer.get_vocabulary())
